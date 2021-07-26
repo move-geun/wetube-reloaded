df77c85f1d5b4fe437063a1a8a47dfd9fbbbaf80
move-geun <movegeun11@gmail.com>
2021-07-26 23:26:23 +0900

    add many pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,9 @@
-export const home = (req, res) => res.render("home");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send(`Video Edit #${req.params.id}`);
-};
-export const search = (req, res) => res.send("Search Video");
-export const upload = (req, res) => res.send("Upload Video");
-export const deleteVideo = (req, res) => {
-  return res.send(`Delete Video #${req.params.id}`);
-};
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("see", { pageTitle: "See" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const deleteVideo = (req, res) =>
+  res.render("remove", { pageTitle: "Delete" });
--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -6,6 +6,6 @@ const videoRouter = express.Router();
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", see);
 videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)/remove", deleteVideo);
 
 export default videoRouter;
