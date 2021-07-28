8efd36121713260d8279c121c92266879d2b2739
move-geun <movegeun11@gmail.com>
2021-07-28 20:17:35 +0900

    plus edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,43 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("see", { pageTitle: "See" });
+const fakeUser = {
+  userName: "nico",
+  loggedIn: false,
+};
+
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    view: 26,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    view: 26,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    view: 1,
+    id: 3,
+  },
+];
+
+export const home = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) =>
   res.render("search", { pageTitle: "Search" });
