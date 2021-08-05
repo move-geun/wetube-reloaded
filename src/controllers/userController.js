d21f8ca7e4d1d0b688540699b6cfbe0f8feff6d0
move-geun <movegeun11@gmail.com>
2021-08-05 19:08:15 +0900

    compare user password & db password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,28 +1,56 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   if (password !== password2) {
     const errormessage = "비밀번호가 맞지 않습니다.";
-    return res.render("join", { pageTitle: "Join", errormessage });
+    return res.status(400).render("join", { pageTitle: "Join", errormessage });
   }
   const exists = await User.exists({ $or: [{ name }, { email }] });
   if (exists) {
-    const errormessage = "이미 존재하는 ID or Email 입니다.";
-    return res.render("join", { pageTitle: "Join", errormessage });
+    const errormessage = "이미 존재하는 Name or Email 입니다.";
+    return res.status(400).render("join", { pageTitle: "Join", errormessage });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("upload", {
+      pageTitle: "Join",
+      errorMessage: error._message,
+    });
+  }
 };
+
 export const edit = (req, res) => res.send("User Edit");
 export const remove = (req, res) => res.send("Delete User");
-export const login = (req, res) => res.send("Log In");
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle: "Log In",
+      errormessage: "해당 Username으로 가입한 계정이 없습니다.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle: "Log In",
+      errormessage: "틀린 Password입니다.",
+    });
+  }
+  res.redirect("/");
+};
 export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("Watch video");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,7 @@ export const home = async (req, res) => {
     const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", { pageTitle: "Home", videos });
   } catch {
-    return res.render("404", { pageTitle: "Video not found" });
+    return res.status(404).render("404", { pageTitle: "Video not found" });
   }
 };
 
@@ -13,7 +13,7 @@ export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (video === null) {
-    return res.render("404", { pageTitle: "Video not found" });
+    return res.status(404).render("404", { pageTitle: "Video not found" });
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
@@ -22,7 +22,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (video === null) {
-    return res.render("404", { pageTitle: "Video not found" });
+    return res.status(404).render("404", { pageTitle: "Video not found" });
   }
   return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 };
@@ -32,7 +32,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (video === null) {
-    return res.render("404", { pageTitle: "Video not found" });
+    return res.status(404).render("404", { pageTitle: "Video not found" });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -57,7 +57,7 @@ export const postUpload = async (req, res) => {
     return res.redirect("/");
   } catch (error) {
     console.log(error);
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: `Upload Video`,
       errorMessage: error._message,
     });
