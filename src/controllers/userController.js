c16ad55d81bad39d208f664aa9fd802978dd5c66
move-geun <movegeun11@gmail.com>
2021-08-05 17:43:34 +0900

    bcypt & check duplicated contents

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,16 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  if (password !== password2) {
+    const errormessage = "비밀번호가 맞지 않습니다.";
+    return res.render("join", { pageTitle: "Join", errormessage });
+  }
+  const exists = await User.exists({ $or: [{ name }, { email }] });
+  if (exists) {
+    const errormessage = "이미 존재하는 ID or Email 입니다.";
+    return res.render("join", { pageTitle: "Join", errormessage });
+  }
   await User.create({
     name,
     username,
